58da7e16ff94ad29855e2ecef97d6eca004f7247
usjbro <30867715+usjbro@users.noreply.github.com>
2025-02-14 12:30:22 -0500

    day 4 tasks completed

--- day4_task.py
+++ day4_task.py
@@ -12,3 +12,26 @@
 #     print("Tails")
 
 # Lists
+# states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+# states_of_america[1] = "Pencilvania"
+# states_of_america.extend(["Angelaland", "Jack Bauer Land"])
+# for x in states_of_america:
+#     print(x, end="\n")
+#  print(states_of_america[49])
+
+# import random
+# friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+# # option 1
+# print(random.choice(friends))
+
+# # option 2
+# random_index = random.randint(0, 4)
+# print(friends[random_index])
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+ 
+dirty_dozen = [fruits, vegetables]
+ 
+print(dirty_dozen[1][1])
