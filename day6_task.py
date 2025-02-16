ebf7816d8de49c34e321003727369281904fd6fe
usjbro <30867715+usjbro@users.noreply.github.com>
2025-02-16 09:59:22 -0500

    Day6 tasks

---
+++ day6_task.py
@@ -0,0 +1,20 @@
+# Functions
+# print("Hello")
+# num_char = len("Hello")
+# print(num_char)
+
+# define function
+# def my_function():
+#     print("Hello")
+#     print("Bye")
+# my_function()
+
+# For function
+# for item in list_of_items:
+#     # Do Something
+# for number in range(a, b):
+#     print(number)
+
+# While Function
+# while something_is_true:
+#     print("Something")
