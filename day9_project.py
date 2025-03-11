619a315cc9bb2b90b51b5d36f83b7d3df6503839
usjbro <30867715+usjbro@users.noreply.github.com>
2025-03-11 09:49:32 -0400

    day 9 project complete

--- day9_project.py
+++ day9_project.py
@@ -7,18 +7,17 @@ bids = True
 def bid():
     bidder_input = input("What is your name:bid?\n")
     bidder, amount = bidder_input.split(":", 1)
-    bid_dict[bidder.strip()] = amount.strip()
+    bid_dict[bidder.strip().lower()] = int(amount.strip())
     new_bid = input("Are there more bidders 'Y/y' for Yes or 'N/n' for No? ").lower()
     if new_bid == "y":
-        return bids, bid_dict
+        return True, bid_dict
     else:
-        print(new_bid)
-        bids = False
-        return bids, bid_dict
+        return False, bid_dict
         
 while bids:
     bids, bid_dict = bid()
 
+print(bid_dict)
 winning_bidder = max(bid_dict, key=bid_dict.get)
 winning_bid = bid_dict[winning_bidder]
-print(f"The winnin bid is {winning_bid} by {winning_bidder}")
+print(f"The winning bid is ${winning_bid} by {winning_bidder}")
