419778b9d382195f917a37b2aa24f1e40b8f2a06
usjbro <30867715+usjbro@users.noreply.github.com>
2025-03-12 15:41:12 -0400

    added docstring to format_name function

--- day10_task.py
+++ day10_task.py
@@ -4,20 +4,21 @@
 
 # output = my_function()
 
-# def format_name(f_name, l_name):
-#     return f_name.title(), l_name.title()
+def format_name(f_name, l_name):
+    """Take a first and last name and format it to return the title case version of the name."""
+    return f"f_name.title() l_name.title()"
 
-# formated_name = format_name("asdf","aslHJ")
+formated_name = format_name("asdf","aslHJ")
 
-# print(type(formated_name))
-# print(formated_name[0] + " " + formated_name[1])
+print(type(formated_name))
+print(formated_name)
 
-def is_leap_year(year):
-    return (year % 4 == 0 and year % 400 != 0) or year % 100 == 0
+# def is_leap_year(year):
+#     return (year % 4 == 0 and year % 400 != 0) or year % 100 == 0
 
-year = int(input("Leap Year Checker: "))
+# year = int(input("Leap Year Checker: "))
 
-if is_leap_year(year):
-    print("Leap Year!")
-else:
-    print("Not a Leap Year")
+# if is_leap_year(year):
+#     print("Leap Year!")
+# else:
+#     print("Not a Leap Year")
