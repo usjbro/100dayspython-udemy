ee2ca9bcb832b0ef636d334db721772ec655537a
usjbro <30867715+usjbro@users.noreply.github.com>
2025-03-13 12:44:45 -0400

    day 10 project completed

--- day10_project.py
+++ day10_project.py
@@ -1,15 +1,43 @@
 print("Calculator")
 
-first_num = float(input("First number: "))
-second_num = float(input("Second number: "))
-operation = input("Pick an operation:\n+\n-\n*\n/\n")
-if operation == "+":
-    print(f"The {first_num} + the {second_num} = ", first_num + second_num)
-elif operation == "-":
-    print(f"The {first_num} - the {second_num} = ", first_num - second_num)
-elif operation == "*":
-    print(f"The {first_num} * the {second_num} = ", first_num * second_num)
-elif operation == "/":
-    print(f"The {first_num} / the {second_num} = ", first_num / second_num)
-else:
-    print("Invalid operator entered")
+def add(first_num, second_num):
+    return first_num + second_num
+    # result = first_num + second_num
+    # print(f"The {first_num} + the {second_num} = {result}")
+
+
+def subtract(first_num, second_num):
+    return first_num - second_num
+    # result = first_num - second_num
+    # print(f"The {first_num} - the {second_num} = {result}")
+
+
+def multi(first_num, second_num):
+    return first_num * second_num
+    # result = first_num * second_num
+    # print(f"The {first_num} * the {second_num} = {result}")
+
+
+def divide(first_num, second_num):
+    return first_num / second_num
+    # result = first_num / second_num
+    # print(f"The {first_num} / the {second_num} = {result}")
+
+
+operations = {"+": add, "-": subtract, "*": multi, "/": divide}
+
+choice = "n"
+while choice != "q":
+    if choice == "n":
+        first_num = float(input("First number: "))
+    second_num = float(input("Second number: "))
+    operation = input("Pick an operation:\n+\n-\n*\n/\n")
+    if operation in operations:
+        answer = operations[operation](first_num, second_num)
+        print(first_num, operation, second_num, " = ", str(answer))
+        choice = input(print(f"(y) to contine with {answer} or (n) to pick new first number or (q) to quit"))
+        if choice == "y":
+            print(f"First number: {answer}")
+            first_num = answer
+    else:
+        print("Unknown operation")
