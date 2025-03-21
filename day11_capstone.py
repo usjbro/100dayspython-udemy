80709c3a20fec070c3b8774b02cfe0ef62f3125e
usjbro <30867715+usjbro@users.noreply.github.com>
2025-03-21 09:58:39 -0400

    final update to day 11 blackjack

--- day11_capstone.py
+++ day11_capstone.py
@@ -1,12 +1,9 @@
 import random
 import art
 
-print(art.logo)
-
 def deal_card():
     cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
-    card = random.choice(cards)
-    return card
+    return random.choice(cards)
 
 def calc_score(cards):
     if sum(cards) == 21 and len(cards) == 2:
@@ -24,57 +21,50 @@ def determine_winner(player_score, dealer_score):
     print(f"Dealer's final score: {dealer_score}")
 
     if player_score > 21:
-        print("Player busts! Dealer wins.")
-        is_game_over = True
+        return("Player busts! Dealer wins.")
     elif dealer_score > 21:
-        print("Dealer busts! Player wins.")
-        is_game_over = True
+        return("Dealer busts! Player wins.")
     elif player_score == dealer_score:
-        print("It's a draw!")
-        is_game_over = True
+        return("It's a draw!")
     elif player_score == 21:
-        print("Player wins with a Blackjack!")
-        is_game_over = True
+        return("Player wins with a Blackjack!")
     elif dealer_score == 21:
-        print("Dealer wins with a Blackjack!")
-        is_game_over = True
+        return("Dealer wins with a Blackjack!")
     elif player_score > dealer_score:
-        print("Player wins!")
-        is_game_over = True
+        return("Player wins!")
     else:
         print("Dealer wins!")
 
-player_cards = []
-dealer_cards = []
-computer_score = -1
-player_score = -1
-is_game_over = False
+def play_game():
+    player_cards = [deal_card(), deal_card()]
+    dealer_cards = [deal_card(), deal_card()]
+    
+    is_game_over = False
 
-for _ in range(2):
-    player_cards.append(deal_card())
-    dealer_cards.append(deal_card())
+    while not is_game_over:
+        player_score = calc_score(player_cards)
+        dealer_score = calc_score(dealer_cards)
 
-while not is_game_over:
-    player_score = calc_score(player_cards)
-    dealer_score = calc_score(dealer_cards)
+        print(f"Your cards are: {player_cards}, current score: {player_score}")
+        print(f"Dealers first card is {dealer_cards[0]}")
+        # print(f"Dealers cards: {dealer_cards}") #Debug show dealers cards
 
-    print(f"Your cards are: {player_cards}, current score: {player_score}")
-    print(f"Dealers first card is {dealer_cards[0]}")
-    # print(f"Dealers cards: {dealer_cards}") #Debug show dealers cards
-
-
-    if player_score == 0 or dealer_score == 0 or player_score > 21:
-        is_game_over = True
-    else:
-        player_continue = input("Do you hand to (h)it or (s)tand? ")
-        if player_continue == "h":
-            player_cards.append(deal_card())
-            player_score = calc_score(player_cards)
-        else:
+        if player_score == 0 or dealer_score == 0 or player_score > 21:
             is_game_over = True
+        else:
+            player_continue = input("Do you hand to (h)it or (s)tand? ")
+            if player_continue == "h":
+                player_cards.append(deal_card())
+            else:
+                is_game_over = True
 
-while dealer_score != 0 and dealer_score < 17:
-    dealer_cards.append(deal_card())
+    while calc_score(dealer_cards) < 17:
+        dealer_cards.append(deal_card())
+
     dealer_score = calc_score(dealer_cards)
+    print(determine_winner(player_score, dealer_score))
 
-print(determine_winner(player_score, dealer_score))
+while input("Do you want to play blackjack (y)yes or (n)no? ").lower() == "y":
+    print("\n"* 20)
+    print(art.logo)
+    play_game()
